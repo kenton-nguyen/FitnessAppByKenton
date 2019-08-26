b715d4239287755ba9dbfb451c5cb30bb05da2a8
kenton <knguy196@ucsc.edu>
2019-08-25 20:08:13 -0700

    youtube API added

--- app/src/main/java/com/example/myapplication/YoutubeConfig.java
+++ app/src/main/java/com/example/myapplication/YoutubeConfig.java
@@ -6,7 +6,7 @@ public class YoutubeConfig {
 
     }
 
-    private static final String API_KEY = "AIzaSyBk8ULPxAAQvXfU9Gc2qpMwySl-5-HVZu0";
+    private static final String API_KEY = "";
 
     public static String getApiKey(){ // because API_KEY is private
         return API_KEY;
